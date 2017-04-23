5a4af8edda230ae152177b5d6b312b958dc2bded
William Roberts <wroberts@tresys.com>
2017-04-23 09:02:28 +0200

    Import common sepolicy
    Change-Id: I28a4aaf6c126535f0a88001582641b234a750015

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,2 +1,10 @@
 BOARD_SEPOLICY_DIRS += \
     vendor/aoscp/sepolicy
+	
+BOARD_SEPOLICY_UNION += \
+    file_contexts \
+    file.te \
+    genfs_contexts \
+    installd.te \
+    mac_permissions.xml \
+	vold.te
