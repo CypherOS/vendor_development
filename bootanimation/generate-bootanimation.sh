a7d558a6e0d0e70cafd5fe13b2ed832d246e472f
Mati Halperin <matias.halperin@gmail.com>
2017-04-30 19:40:29 -0300

    Correct bootanimation
    Aaron's method broke the bootanimation. So I cleaned up, used some
    things introduced by Aaron, and removed some others that were useless.
    Now it works flawlessly, and it's more optimized than Aaron's method.
    
    This should be merged before release
    
    Change-Id: I858cca8a393822c07ab187037fcb13f8cad38a9b

--- bootanimation/generate-bootanimation.sh
+++ bootanimation/generate-bootanimation.sh
@@ -4,42 +4,34 @@ WIDTH="$1"
 HEIGHT="$2"
 HALF_RES="$3"
 
-ORIGINAL_WIDTH="1440"
-ORIGINAL_HEIGHT="1440"
-
-PARTS="0 1"
 FPS="60"
 
 OUT="$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION"
 
-if [ "${HALF_RES}" == "true" ]; then
-    WIDTH=$(($WIDTH / 2))
-    HEIGHT=$(($HEIGHT / 2))
+# Check if HALF_RES
+if [ "$HALF_RES" = true ]
+then
+    WIDTH=$(("$WIDTH" / 2))
+    HEIGHT=$(("$HEIGHT" / 2))
 fi
 
-#Set image size
-RESOLUTION="${WIDTH}^x${HEIGHT}^>"
-
-#Create a folder for each part
-PARTS=$(echo "$PARTS" | tr " " "\n")
-
-rm -rf "$OUT/bootanimation/"
-
-for PART in $PARTS
-do
-    mkdir -p "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/part$PART"
-done
-
 # Decompress bootanimation.zip
-unzip "vendor/aoscp/bootanimation/bootanimation.zip" -d "$OUT/bootanimation/"
+unzip -q vendor/aoscp/bootanimation/bootanimation.zip -d "$OUT/bootanimation"
 
 # Resize all images
-find $OUT/bootanimation/ -type f -name '*.png' -exec convert -resize "$RESOLUTION" -colors 250 {}
+for IMAGE in "$OUT/bootanimation/"*"/"*".png"
+do
+    convert "$IMAGE" -resize "${WIDTH}x$HEIGHT" -colors 250 "$IMAGE"
+done
+
+# Get width and height of all the images
+WIDTH=$(identify -ping -format '%w ' "$OUT/bootanimation/"*"/"*".png" | cut -d ' ' -f 1)
+HEIGHT=$(identify -ping -format '%h ' "$OUT/bootanimation/"*"/"*".png" | cut -d ' ' -f 1)
 
 # Create desc.txt
-echo "$IMAGEWIDTH $IMAGEHEIGHT" "$FPS" > "$OUT/bootanimation/desc.txt"
-cat "vendor/aoscp/bootanimation/desc.txt" >> "$OUT/bootanimation/desc.txt"
+echo "$WIDTH $HEIGHT $FPS" > "$OUT/bootanimation/desc.txt"
+cat vendor/aoscp/bootanimation/desc.txt >> "$OUT/bootanimation/desc.txt"
 
 # Create bootanimation.zip
-cd "${OUT}/bootanimation"
-find -type f | sort | zip -@ -0 "${OUT}/bootanimation.zip"
+cd "$OUT/bootanimation"
+zip -q -r -0 "$OUT/bootanimation.zip" .
