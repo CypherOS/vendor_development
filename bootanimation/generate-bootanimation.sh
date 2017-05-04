aca1e5aa2831721e893faea1ab34fdf46c5b5e20
Mati Halperin <matias.halperin@gmail.com>
2017-05-04 15:19:47 -0300

    Reimplement new way of generating bootanimation
    Change-Id: I301331193bc025a52681858f70bb40e3dcc78311

---
+++ bootanimation/generate-bootanimation.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+WIDTH=$1
+HEIGHT=$2
+HALF_RES=$3
+
+FPS=60
+
+OUT="$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION"
+
+# Reduce image size by 50% if HALF_RES
+if [ "$HALF_RES" = true ]
+then
+    WIDTH=$(("$WIDTH" / 2))
+    HEIGHT=$(("$HEIGHT" / 2))
+fi
+
+# Decompress bootanimation.zip
+unzip -q vendor/aoscp/bootanimation/bootanimation.zip -d "$OUT/bootanimation"
+
+# Resize all images
+mogrify -resize "${WIDTH}x$HEIGHT" -colors 250 "$OUT/bootanimation/"*"/"*".png"
+
+# Get width and height of all the images
+WIDTH=$(identify -ping -format '%w ' "$OUT/bootanimation/"*"/"*".png" | cut -d ' ' -f 1)
+HEIGHT=$(identify -ping -format '%h ' "$OUT/bootanimation/"*"/"*".png" | cut -d ' ' -f 1)
+
+# Restore size to include it in desc.txt if HALF_RES
+if [ "$HALF_RES" = true ]
+then
+    WIDTH=$(("$WIDTH" * 2))
+    HEIGHT=$(("$HEIGHT" * 2))
+fi
+
+# Create desc.txt
+echo "$WIDTH $HEIGHT $FPS" > "$OUT/bootanimation/desc.txt"
+cat vendor/aoscp/bootanimation/desc.txt >> "$OUT/bootanimation/desc.txt"
+
+# Create bootanimation.zip
+cd "$OUT/bootanimation"
+zip -q -r -0 "$OUT/bootanimation.zip" .
+
