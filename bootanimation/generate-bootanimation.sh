8ebe65433be3c86139d1e569ae74c9f0302c73d3
Mati Halperin <matias.halperin@gmail.com>
2017-09-09 23:51:19 -0400

    vendor: add support for advanced bootanimation conversion
    The new implementation has the following advantages:
      - No need to generate all the files again when a change is made to
        the bootanimation
      - Reduces the size of vendor_aoscp
      - No need to resize all the images to all the sizes, only generate
        the maximum resolution
      - Any resolution is available, not only the preset ones (always the
        same or lower than the uploaded one)
    
    Changes from LineageOS:
      - Support any aspect ratio, number of parts and fps
      - Optimize and improve syntax
    
    Based on:
    https://github.com/LineageOS/android_vendor_lineage/tree/lineage-15.0/bootanimation
    
    Change-Id: Id5db260dfc4d9e60669b26b2159c8f26c82a61b0

---
+++ bootanimation/generate-bootanimation.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+WIDTH=$1
+HEIGHT=$2
+HALF_RES=$3
+
+FPS=60
+
+OUT="$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION"
+
+# Halve image size if HALF_RES
+if [ "$HALF_RES" = true ]
+then
+    WIDTH=$((WIDTH / 2))
+    HEIGHT=$((HEIGHT / 2))
+fi
+
+# Create a folder for each part
+PARTS=$(grep . -c < "vendor/aoscp/bootanimation/desc.txt")
+
+while [ "$PARTS" -gt 0 ]
+do
+    PARTS=$((PARTS-1))
+    mkdir -p "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/part$PARTS"
+done
+
+# Decompress bootanimation.zip
+tar xfp "vendor/aoscp/bootanimation/bootanimation.tar" -C "$OUT/bootanimation/"
+
+# Resize all images
+mogrify -resize "${WIDTH}x${HEIGHT}>" -colors 250 "$OUT/bootanimation/"*"/"*".png"
+
+# Get new width and height
+WIDTH=$(identify -format "%w " "$OUT/bootanimation/"*"/"*".png" | cut -d ' ' -f 1)
+HEIGHT=$(identify -format "%h " "$OUT/bootanimation/"*"/"*".png" | cut -d ' ' -f 1)
+
+# Duplicate image size if HALF_RES
+if [ "$HALF_RES" = true ]
+then
+    WIDTH=$((WIDTH * 2))
+    HEIGHT=$((HEIGHT * 2))
+fi
+
+# Create desc.txt
+echo "$WIDTH $HEIGHT $FPS" > "$OUT/bootanimation/desc.txt"
+cat "vendor/aoscp/bootanimation/desc.txt" >> "$OUT/bootanimation/desc.txt"
+
+# Create bootanimation.zip
+cd "$OUT/bootanimation"
+zip -q -r -0 "$OUT/bootanimation.zip" .
