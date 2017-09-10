297469d3630adc8f57ce999d6440fec1fd8878b7
Mati Halperin <matias.halperin@gmail.com>
2017-09-09 23:51:19 -0400

    vendor: add support for advanced bootanimation conversion
    The new implementation has the following advantages:
      - No need to generate all the files again when a change is made to
        the bootanimation
      - Reduces the size of vendor_aoscp
      - No need to resize all the images to all the sizes, only generate
        the maximum resolution
      - Any resolution is available, not only the preset ones (always the
        same or lower than the uploaded one)
    
    Changes from LineageOS:
      - Support any number of parts and fps
      - Use zip file instead of tar
    
    Based on:
    https://github.com/LineageOS/android_vendor_lineage/tree/lineage-15.0/bootanimation
    
    Change-Id: Id5db260dfc4d9e60669b26b2159c8f26c82a61b0

---
+++ bootanimation/generate-bootanimation.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+WIDTH=$1
+HEIGHT=$2
+HALF_RES=$3
+
+FPS=60
+
+OUT="$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION"
+
+if [ "$HEIGHT" -lt "$WIDTH" ]; then
+    IMAGEWIDTH="$HEIGHT"
+else
+    IMAGEWIDTH="$WIDTH"
+fi
+
+IMAGESCALEWIDTH="$IMAGEWIDTH"
+IMAGESCALEHEIGHT=$(expr $IMAGESCALEWIDTH / 3)
+
+if [ "$HALF_RES" = "true" ]; then
+    IMAGEWIDTH=$(expr $IMAGEWIDTH / 2)
+fi
+
+IMAGEHEIGHT=$(expr $IMAGEWIDTH / 3)
+
+RESOLUTION=""$IMAGEWIDTH"x"$IMAGEHEIGHT""
+
+unzip -q "vendor/aoscp/bootanimation/bootanimation.zip" -d "$OUT/bootanimation/"
+
+mogrify -resize $RESOLUTION -colors 250 "$OUT/bootanimation/"*"/"*".png"
+
+# Create desc.txt
+echo "$IMAGESCALEWIDTH $IMAGESCALEHEIGHT $FPS" > "$OUT/bootanimation/desc.txt"
+cat "vendor/aoscp/bootanimation/desc.txt" >> "$OUT/bootanimation/desc.txt"
+
+# Create bootanimation.zip
+cd "$OUT/bootanimation"
+
+zip -qr0 "$OUT/bootanimation.zip" .
