826f75d95ece96701bb2b469c4ebdbcb86be1121
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-08-20 04:29:44 -0400

    prebuilt: Switch to Google soundpicker
    Change-Id: I881eb9a7a601067f80e9afb0586eac0ee54b8a77

--- configs/packages.mk
+++ configs/packages.mk
@@ -27,7 +27,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += COTA
 
 # Google Packages
-PRODUCT_PACKAGES += Wallpapers
+PRODUCT_PACKAGES += \
+    SoundPickerPrebuilt \
+	Wallpapers
 
 # Extra tools
 PRODUCT_PACKAGES += \
